950cd3097875f607c9eeee699d7100b4546cd735
Tomáš L <tomas.haluzar@seznam.cz>
2018-06-28 18:27:53 +0200

    edited

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
-var downloadingImage = new Image();
-
-downloadingImage.onload = function(){
-  document.querySelector("#universelogo").src = this.src; 
-  document.querySelector("#content").style.opacity = "1";
-};
-
-downloadingImage.src = "src/logo.png";
+window.onload = function(){
+  setTimeout(function()
+  	{document.querySelector("#content").style.opacity = "1";}, 400); };
